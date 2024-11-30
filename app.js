9839eeb08556ea55bf9da35e110ba8057a0eaf6b
Lee Chanhyeong <lch2023@naver.com>
2024-11-30 03:34:29 +0000

    Init mongoDB 세팅

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const path = require('path');
+const { MongoClient } = require('mongodb');
 
 const app = express();
-app.listen(8080, () => {
-  console.log('App listening on port 8080.');
-});
 
 app.use(express.static('public'));
+
+let db;
+const url =
+  'mongodb+srv://lch2023:aa**895623@cluster0.y9ddq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+new MongoClient(url)
+  .connect()
+  .then((client) => {
+    console.log('DB is connected');
+    db = client.db('studygram');
+    app.listen(8080, () => {
+      console.log('App listening on port 8080.');
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
